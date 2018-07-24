3f7d7cef337ef6e72a10ca5fde58bc686f15cb86
gracehillen <grahillen19@ahastars.org>
2018-07-24 18:48:18 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,13 +1,15 @@
 def simple_curfew_checker(time)
   if time >= 11
     puts "past curfew"
-  else 
-    puts "it's not curfew yet"
   end
 end
 
 def curfew_checker(time)
-  # code goes here
+    if time >= 11
+    puts "past curfew"
+  else 
+    puts "it's not curfew yet"
+  end
 end
 
 def complex_curfew_checker(time)
